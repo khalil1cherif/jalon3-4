10b7c288af6fc539803d2ea43fa85eb55e3a1053
khalil1cherif <73797449+khalil1cherif@users.noreply.github.com>
2022-11-14 14:21:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,35 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import streamlit as st
-
-import pandas as pd
-import numpy as np
+import pickle
 import prediction
+import streamlit as st
+from PIL import Image
 
-# Presentation de l'application
+image = Image.open('pic.png')
 
-st.title('Application : RevieWAnalyzer')
+model= pickle.load(open('model_file.pkl', 'rb'))
+vectorizer= pickle.load(open('vectoriseur_file.vec', 'rb'))
 
-monlabel = "Quel texte analyser ? "
-options = pd.DataFrame(['Avis dataset', 'Texte libre'])
 
-n_topics = st.number_input(label="Le nombre de topics", min_value=1,max_value=15)  # st.number_input("topic number", value=0)#st.number_input(label= "Le nombre de topics", min_value=0, max_value=15)
-#with st.sidebar:
-st.radio(monlabel, options)
-text = st.text_input(label="Donnez nous votre avis")
+# Add a title and intro text
+st.title('Topics extractor')
+st.text('This is a web app that allows Topics extraction from bad restaurant reviews')
 
-if st.button(label="Détecter le sujet d'insatisfaction") == True:
-    prediction.predict_topics(prediction.model, prediction.vectoriseur, n_topics,text)
+
+
+
+with st.form("my_form"):
+   st.image(image)
+
+   number = st.number_input("topics number", value=1, min_value=1, max_value=15)
+   text=st.text_input("Your review", placeholder="At least 4 characters")
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Extract topics")
+   if submitted:
+      if len(text) < 5:
+         st.error("Invalid text: At least 4 characters")
+      else:
+         pre=prediction.predict_topics(model, vectorizer,number,text)
+         if not pre[1]:
+            st.info("Polarity score: "+ str(pre[0]))
+         else:
+            st.info("Polarity score: "+ str(pre[0]))
+            st.info("Topics: "+ str(pre[1]))
